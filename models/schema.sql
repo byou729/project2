4bf88db0b497920d95f82992d8f1419c91dd8e9b
JJbas <jjbastien78@gmail.com>
2018-08-20 17:37:39 -0500

    i added tables

--- models/schema.sql
+++ models/schema.sql
@@ -1,48 +1,63 @@
 DROP DATABASE IF EXISTS wine;
-CREATE DATABASE wine;
+CREATE DATABASE wines_db;
 
-use wine;
-INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+use wine_db;
+CREATE TABLE wines(
+    id INTEGER , 
+    wbarcodeId INTEGER NOT NULL,
+    wname VARCHAR, 
+    wtype VARCHAR,
+    wmajorcat VARCHAR 
+);
+INSERT INTO wines (wbarcodeId, wname, wtype, wmajorcat)
 VALUES (111111, "Yellow Tail", "Malbec", "Bold Red");
 
-INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+INSERT INTO wines (wbarcodeId, wname, wtype, wmajorcat)
 VALUES (111111, "Yellow Tail", "Merlot", "Medium Red");
 
-INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+INSERT INTO wines (wbarcodeId, wname, wtype, wmajorcat)
 VALUES (111111, "Yellow Tail", "Pinot Norir", "Light Red");
 
-INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+INSERT INTO wines (wbarcodeId, wname, wtype, wmajorcat)
 VALUES (111111, "Yellow Tail", "Provencai Rose", "Rose");
 
-INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+INSERT INTO wines (wbarcodeId, wname, wtype, wmajorcat)
 VALUES (111111, "Yellow Tail", "Chardonay", "Rich White");
 
-INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+INSERT INTO wines (wbarcodeId, wname, wtype, wmajorcat)
 VALUES (111111, "Yellow Tail", "Sauvignon Blanc", "Light White");
 
-INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+INSERT INTO wines (wbarcodeId, wname, wtype, wmajorcat)
 VALUES (111111, "Yellow Tail", "Champagne", "Sparking");
 
-INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+INSERT INTO wines (wbarcodeId, wname, wtype, wmajorcat)
 VALUES (111111, "Yellow Tail", "Mocato", "Sweet White");
 
-INSERT INTO wines (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+INSERT INTO wines (wbarcodeId, wname, wtype, wmajorcat)
 VALUES (111111, "Yellow Tail", "Port", "Desert");
 
-INSERT INTO food (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+CREATE TABLE food (
+  fbarcodeId INTEGER NOT NULL,
+    food_name VARCHAR,
+    food_category VARCHAR, 
+    perfect_Pair VARCHAR,
+    ok_Pair VARCHAR
+)
+
+INSERT INTO food (fbarcodeId, food_name, food_category, perfectpair, okpair)
 VALUES (111111, "Beef", "Red Meat", "Bold Red", "Medium Red");
 
-INSERT INTO food (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+INSERT INTO food (fbarcodeId, food_name, food_category, perfectpair, okpair)
 VALUES (111111, "Soft Cheese & Cream", "Diary", "Light Red", "Rich White");
 
-INSERT INTO food (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+INSERT INTO food (fbarcodeId, food_name, food_category, perfectpair, okpair)
 VALUES (111111, "Onion", "Vagetable", "Medium Red", "Light Red");
 
-INSERT INTO food (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+INSERT INTO food (fbarcodeId, food_name, food_category, perfectpair, okpair)
 VALUES (111111, "Chili", "Herbs & Spice", "Medium Red", "Rose");
 
-INSERT INTO food (FBARCODEID, FNAME, FCAT, PERFECTPAIR, OKPAIR)
+INSERT INTO food (fbarcodeId, food_name, food_category, perfectpair, okpair)
 VALUES (111111, "Pasta", "Starch", "Light White", "Sweet White");
 
-INSERT INTO food (FBARCODEID, FNAME, FCAT, PERFECTPAIR, OKPAIR)
+INSERT INTO food (fbarcodeId, food_name, food_category, perfectpair, okpair)
 VALUES (111111, "Strawberry", "Sweet", "Sweet White", "Sparkling");
