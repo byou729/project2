b28361fa0b2daf9b9346501527e44a713e24feb7
Ben You <byou729@gmail.com>
2018-08-19 21:06:55 -0500

    winedb database and wine and food tables

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,48 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS wine;
+CREATE DATABASE wine;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+use wine;
+INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+VALUES (111111, "Yellow Tail", "Malbec", "Bold Red");
+
+INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+VALUES (111111, "Yellow Tail", "Merlot", "Medium Red");
+
+INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+VALUES (111111, "Yellow Tail", "Pinot Norir", "Light Red");
+
+INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+VALUES (111111, "Yellow Tail", "Provencai Rose", "Rose");
+
+INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+VALUES (111111, "Yellow Tail", "Chardonay", "Rich White");
+
+INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+VALUES (111111, "Yellow Tail", "Sauvignon Blanc", "Light White");
+
+INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+VALUES (111111, "Yellow Tail", "Champagne", "Sparking");
+
+INSERT INTO wines (WBARCODEID, WNAME, WTYPE, WMAJORCAT)
+VALUES (111111, "Yellow Tail", "Mocato", "Sweet White");
+
+INSERT INTO wines (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+VALUES (111111, "Yellow Tail", "Port", "Desert");
+
+INSERT INTO food (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+VALUES (111111, "Beef", "Red Meat", "Bold Red", "Medium Red");
+
+INSERT INTO food (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+VALUES (111111, "Soft Cheese & Cream", "Diary", "Light Red", "Rich White");
+
+INSERT INTO food (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+VALUES (111111, "Onion", "Vagetable", "Medium Red", "Light Red");
+
+INSERT INTO food (WBARCODEID, WNAME, FCAT, WTYPE, WMAJORCAT)
+VALUES (111111, "Chili", "Herbs & Spice", "Medium Red", "Rose");
+
+INSERT INTO food (FBARCODEID, FNAME, FCAT, PERFECTPAIR, OKPAIR)
+VALUES (111111, "Pasta", "Starch", "Light White", "Sweet White");
+
+INSERT INTO food (FBARCODEID, FNAME, FCAT, PERFECTPAIR, OKPAIR)
+VALUES (111111, "Strawberry", "Sweet", "Sweet White", "Sparkling");
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,42 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Wine.findAll({}).then(function(dbWine) {
+      res.json(dbWine);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    db.Wine.create(req.body).then(function(dbWine) {
+      res.json(dbWine);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Wine.destroy({ where: { id: req.params.id } }).then(function(dbWine) {
+      res.json(dbWine);
+    });
+  });
+
+  app.get("/api/examples", function(req, res) {
+    db.Food.findAll({}).then(function(dbFood) {
+      res.json(dbFood);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/examples", function(req, res) {
+    db.Food.create(req.body).then(function(dbFood) {
+      res.json(dbFood);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/examples/:id", function(req, res) {
+    db.Food.destroy({ where: { id: req.params.id } }).then(function(dbFood) {
+      res.json(dbFood);
     });
   });
 };
