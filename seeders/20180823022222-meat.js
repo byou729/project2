3f38671a80eed9d44c0f995864acfe57531df567
JJbas <jjbastien78@gmail.com>
2018-08-23 20:50:52 -0500

    added meats

--- seeders/20180823022222-meat.js
+++ seeders/20180823022222-meat.js
@@ -12,7 +12,43 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
-  },
+   return queryInterface.bulkInsert('Meat', [{
+    foodName: "red meat",
+    foodCategory: "meat",
+    perfectPair: "bold red"
+  }], {});
+  
+  return queryInterface.bulkInsert('cured meat', [{
+    foodName: "cured meat",
+    foodCategory: "meat",
+    perfectPair:"bold red"
+  }], {});
+  return queryInterface.bulkInsert('poultry)', [{
+    foodName: "poultry(chicken/duck/turkey",
+    foodCategory: "meat",
+    perfectPair:"bold red"
+  }], {});
+  return queryInterface.bulkInsert('pork', [{
+    foodName: "pork(roast/tenderloin/chop)",
+    foodCategory: "meat",
+    perfectPair:"bold red"
+  }], {});
+  return queryInterface.bulkInsert('mollusk', [{
+    foodName: "mollusk(oyster/mussel/clam)",
+    foodCategory: "meat",
+    perfectPair:"bold red"
+  }], {});
+  return queryInterface.bulkInsert('fish', [{
+    foodName: "fish(tuna/cod/trout/bass",
+    foodCategory: "meat",
+    perfectPair:"bold red"
+  }], {});
+  return queryInterface.bulkInsert('lobster', [{
+    foodName: "lobster & shellfish(prawn/crab)",
+    foodCategory: "meat",
+    perfectPair:"bold red"
+  }], {});
+
 
   down: (queryInterface, Sequelize) => {
     /*
