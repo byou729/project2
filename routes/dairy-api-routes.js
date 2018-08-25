ce3ed4e8cab905c141482c3c68f3d2751f7c1d1c
JJbas <jjbastien78@gmail.com>
2018-08-24 20:45:31 -0500

    api routes

---
+++ routes/dairy-api-routes.js
@@ -0,0 +1,29 @@
+var db = require("../models");
+
+module.exports = function(app) {
+ app.get("/api/dairy", function(req, res) {
+   // Here we add an "include" property to our options in our findAll query
+   // We set the value to an array of the models we want to include in a left outer join
+   // In this case, just db.Post
+   db.Dairy.findAll({
+     include: [db.Wines]
+   }).then(function(dbDairy) {
+     res.json(dbDairy);
+   });
+ });
+
+ app.get("/api/dairy/:id", function(req, res) {
+   // Here we add an "include" property to our options in our findOne query
+   // We set the value to an array of the models we want to include in a left outer join
+   // In this case, just db.Post
+   db.Dairy.findOne({
+     where: {
+       id: req.params.id
+     },
+     include: [db.Wines]
+   }).then(function(dbDairy) {
+     res.json(dbDairy);
+     console.log(result)
+   });
+ });
+};
